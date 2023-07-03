c48085293af5b862de780b0b418b10e8f833d57e
Holypotatka <arector543@gmail.com>
2023-07-03 12:54:00 +0300

    Добавлены аннотации и методы "до","после" всех тестов и каждым тестом

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,18 +1,44 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.*;
 
 class CalcTest {
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("before all");
+    }
 
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("before each");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("after all");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("after each");
+
+    }
     @Test
-    @DisplayName("Проверка суммирования")
+    @DisplayName("Проверка суммирования 1")
     @Timeout(10)
-    void summ() {
+    @Tag("summ")
+    void summ1() {
         Calc calc = new Calc();
         int result = calc.summ(1,6);
         Assertions.assertEquals(7, result, "Не тот ответ");
+    }
+
+    @Test
+    @DisplayName("Проверка суммирования 2")
+    @Timeout(10)
+    void summ2() {
+        Calc calc = new Calc();
+        int result = calc.summ(2,6);
+        Assertions.assertEquals(8, result, "Не тот ответ");
     }
 }
