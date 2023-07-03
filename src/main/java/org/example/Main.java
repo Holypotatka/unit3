df7477ffd10f213239570e6baabe9563c868e445
Holypotatka <arector543@gmail.com>
2023-07-03 12:13:15 +0300

    Вызов summ Calc в Main, исправление опечаток

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calc calculator = new Calc();
+        calculator.summ(1,10);
     }
 }
