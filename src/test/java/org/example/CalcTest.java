401d07045a870342e8e1f6619dc4fb613febe4a4
Holypotatka <arector543@gmail.com>
2023-07-03 13:46:10 +0300

    Добавлен параметризованный тест для сложения и тест сьют для параметризованных тестов

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,44 +1,57 @@
 package org.example;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CalcTest {
     @BeforeAll
-    public static void beforeAll(){
+    public static void beforeAll() {
         System.out.println("before all");
     }
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         System.out.println("before each");
     }
 
     @AfterAll
-    public static void afterAll(){
+    public static void afterAll() {
         System.out.println("after all");
     }
 
     @AfterEach
-    public void afterEach(){
+    public void afterEach() {
         System.out.println("after each");
 
     }
+
     @Test
     @DisplayName("Проверка суммирования 1")
     @Timeout(10)
     @Tag("summ")
     void summ1() {
         Calc calc = new Calc();
-        int result = calc.summ(1,6);
+        int result = calc.summ(1, 6);
         Assertions.assertEquals(7, result, "Не тот ответ");
     }
 
-    @Test
+    @RepeatedTest(3)
     @DisplayName("Проверка суммирования 2")
     @Timeout(10)
     void summ2() {
         Calc calc = new Calc();
-        int result = calc.summ(2,6);
+        int result = calc.summ(2, 6);
         Assertions.assertEquals(8, result, "Не тот ответ");
+    }
+
+    @ParameterizedTest(name = "#{index} - сложение {0} и {1}, ожидаем {2}")
+    @CsvSource({"1,2,3", "-1,2,1", "0,0,0"})
+    @DisplayName("Проверка суммирования 3")
+    @Tag("param")
+    void summ2(int a, int b, int expectedResult) {
+        Calc calc = new Calc();
+        int result = calc.summ(a, b);
+        Assertions.assertEquals(expectedResult, result, "Не тот ответ");
     }
 }
